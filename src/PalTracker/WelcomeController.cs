cf2857a2ec4bd34060b4c071b2738c540ab7c69b
Jimmy Lu <jimmyl@magenic.com>
2018-11-12 11:26:58 -0700

    Scaling Lab

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -2,10 +2,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PalTracker
 {
+
     [Route("/")]
     public class WelcomeController : ControllerBase
     {
+        
+        private readonly WelcomeMessage _message;
+
+        public WelcomeController(WelcomeMessage message)
+        {
+            _message = message;            
+        }
+
+
         [HttpGet]
-        public string SayHello() => "would you like to play a game?";
+        public string SayHello() => _message.Message;
     }
 }
